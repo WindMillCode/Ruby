2014f80a898e853d1bc170757b91ea55fd1961d2
Michael Odumosu <michaelodumosu57@gmail.com>
2020-12-13 09:17:22 -0500

    UPDATE

--- certifications/codeacademy/vids/Classes/class.rb
+++ certifications/codeacademy/vids/Classes/class.rb
@@ -1,23 +1,143 @@
-class Creature
-  def initialize(name)
+# Basic Class
+=begin
+class MyClass
+end
+=end
+
+
+
+
+
+
+
+
+
+
+
+# initialize
+=begin
+class MyClass
+  def initialize name,profession
+    @class = name
+    p %{we have a new profession it is }
+  end
+end
+=end
+
+# a new instance
+=begin
+arc = MyClass.new %{Jake}, %{Carpenter}
+=end
+
+# methods
+=begin
+class MyClass
+  def initialize name,profession
     @name = name
+    p %{we have a new profession it is #{profession}}
   end
-  
-  def fight
-    return "Punch to the chops!"
+
+  def print_name
+    @name
+  end
+
+  def print_profession
+    @profession
+  end  
+end
+
+arc = MyClass.new %{Jake}, %{Carpenter}
+gib = MyClass.new %{Jake}, %{Lawyer}
+arc.print_name 
+arc.print_profession
+=end
+
+
+# global variables
+=begin
+class MyClass
+  $total_professions = 3
+end
+
+# arc = MyClass.new
+p $total_professions
+=end
+
+# class variables
+=begin
+class MyClass
+  @@classname = "Seven"
+  def initialize name,profession
+    @name = name
+    p %{we have a new profession it is #{}}
+  end
+
+  def names
+    p @@classname
+    p @name
   end
 end
 
-# Add your code below!
+arc = MyClass.new
+arc.names
+# p @@classname
+=end
 
-class Dragon < Creature
-  def fight
-    puts "Instead of breathing fire..."
-    return super
+# Inheritance
+=begin
+class Creature
+  def initialize name,profession
+    @name = name
+    p %{we have a new profession it is #{profession} }
   end
 end
 
-arc = Dragon.new %{Jake}
+class Dinosaur < Creature
+end
 
-p arc.fight
-p arc.name
+class TRex < Dinosaur
+end
+
+jak = Dinosaur.new %{Tom}, %{Bucket}
+=end
+
+
+# Override
+=begin
+class MyClass
+  def initialize name,profession
+    @name = name
+    p %{we have a new profession it is }
+  end
+end
+
+class Derived < MyClass
+  def initialize door
+    @door = door
+    p %{Override base class}
+  end  
+end
+
+jak = Derived.new %{Tom} 
+=end
+
+
+# Complement
+=begin
+class MyClass
+  def initialize name,profession
+    @name = name
+    return %{we have a new profession it is }
+  end
+end
+
+class Derived < MyClass
+  def initialize name,profession,door
+    @door = door
+    p super name,profession
+    p "more code"
+  end  
+end
+
+jak = Derived.new %{Tom}, %{Janitor},%{Bucket}
+=end
